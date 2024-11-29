f750257223e4e11133f2cd60b02d731104a4a004
Lucio <lucioew28@gmail.com>
2024-11-29 16:06:42 -0300

    :sparkles: feat: add whatsapp button link

--- apps/web/src/components/modal/index.tsx
+++ apps/web/src/components/modal/index.tsx
@@ -13,6 +13,11 @@ interface ModalProps {
 }
 
 export function Modal({ open, setOpen, prediction }: ModalProps) {
+  const phone = "5527992610099";
+  const text = encodeURIComponent(
+    `Olá! Tudo bem? Fiz uma pré-avaliação do meu imóvel pelo site e obtive o valor estimado de *R$${prediction}*.`
+  );
+  const whatsappList = `https://api.whatsapp.com/send/?phone=${phone}&text=${text}`;
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className="sm:max-w-md">
@@ -26,11 +31,24 @@ export function Modal({ open, setOpen, prediction }: ModalProps) {
             valor do seu imóvel. Confira o resultado abaixo:
           </DialogDescription>
         </DialogHeader>
-        <div className="flex items-center space-x-2 text-2xl font-semibold text-gray-800">
-          {new Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-          }).format(prediction ? prediction : 0)}
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-2 text-2xl font-semibold text-gray-800">
+            {new Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            }).format(prediction ? prediction : 0)}
+          </div>
+
+          <a
+            href={whatsappList}
+            target="_blank"
+            className="flex items-center gap-2"
+          >
+            <img src="/whatsapp.svg" alt="Whatsapp icon" className="w-6 h-6" />
+            <p className="font-semibold text-lg hover:text-green-700 transition-all duration-150">
+              Falar com um corretor
+            </p>
+          </a>
         </div>
       </DialogContent>
     </Dialog>
